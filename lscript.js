0a478e78446b2f8da19c6ecd4bf28dcc52a3b852
Nordall30 <ea.sebusa@gmail.com>
2024-07-15 09:08:05 +0800

    Update lscript.js

--- lscript.js
+++ lscript.js
@@ -98,32 +98,3 @@ links.forEach(link => {
        forms.classList.toggle("show-signup");
     })
 });
-
-// Monitor authentication state
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
-    // User is signed in, you can redirect to the protected page if needed
-    console.log("User is signed in:", user);
-    // Example: redirect to main.html if currently on login.html or signup.html
-    if (window.location.pathname === '/login.html' || window.location.pathname === '/signup.html') {
-      window.location.href = 'Homepage.html';
-    }
-  } else {
-    // No user is signed in, redirect to login page if needed
-    console.log("No user is signed in.");
-    // Example: redirect to login.html if currently on a protected page
-    if (window.location.pathname !== '/login.html' && window.location.pathname !== '/signup.html') {
-      window.location.href = 'login.html';
-    }
-  }
-});
-
-// Function to log out
-function logout() {
-  firebase.auth().signOut().then(() => {
-    console.log('User signed out.');
-    window.location.href = 'login.html'; // Redirect to login page after logout
-  }).catch((error) => {
-    console.error('Sign out error:', error);
-  });
-}
