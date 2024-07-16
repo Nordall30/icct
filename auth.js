cd3d4e829c50bba461b7b2137ed284f3af1a59d1
Nordall30 <ea.sebusa@gmail.com>
2024-07-16 12:20:21 +0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDYrcXkSzYNauCY6QypJKtNk_IVHYmn6Gs",
+    authDomain: "login-fc3db.firebaseapp.com",
+    databaseURL: "https://login-fc3db-default-rtdb.firebaseio.com",
+    projectId: "login-fc3db",
+    storageBucket: "login-fc3db.appspot.com",
+    messagingSenderId: "363044743065",
+    appId: "1:363044743065:web:a50a43257c1a12b9955508"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Check authentication state
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        // No user is signed in, redirect to login page
+        window.location.href = 'index.html';
+    }
+});
